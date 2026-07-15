92e5d846b648821c6b287df643d146a0b51ff43a
Melanie <ruohongwei.wrh@gmail.com>
2026-07-15 10:11:59 +0100

    create a new project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace loops_collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
