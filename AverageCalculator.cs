42467971c63db7455bff93ede568591706aea9d9
Kabir Suri <r.surii1309@gmail.com>
2026-07-15 10:40:48 +0100

    We have completed task 1 to 5

---
+++ AverageCalculator.cs
@@ -0,0 +1,33 @@
+﻿
+namespace loops_collections
+{
+    internal class AverageCalculator
+    {
+        internal void AveragesWithDoWhile()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void AveragesWithFor()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void AveragesWithWhile()
+        {
+            double total = 0.0;
+            int count = 0;
+
+            Console.Write("Enter the first number, or Q to quit: ");
+            string input = Console.ReadLine();
+            while (input.ToUpper() != "Q")
+            {
+                total += double.Parse(input);
+                count++;
+
+                Console.Write("Enter another number, or Q to quit: ");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
