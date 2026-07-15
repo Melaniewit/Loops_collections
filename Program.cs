ce50d0daea2442667d12c4b7cbbd5b260960869b
Kabir Suri <r.surii1309@gmail.com>
2026-07-15 10:33:16 +0100

    gf

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace loops_collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AverageCalculator calculator = new AverageCalculator();
+
+            calculator.AveragesWithWhile();
+            Console.WriteLine("==========");
+
+            calculator.AveragesWithDoWhile();
+            Console.WriteLine("==========");
+
+            calculator.AveragesWithFor();
+        }
+    }
+}
